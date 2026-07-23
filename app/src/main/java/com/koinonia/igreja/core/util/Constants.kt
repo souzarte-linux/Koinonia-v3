6ea082e5ab8d91fafec0fb8d5a1f7b6714d350e2
Fernando Souza <souzaesouzaassociados@gmail.com>
2026-07-23 20:28:19 -0300

    feat: integrate Supabase client with configuration constants and secure credentials via gitignore updates

--- app/src/main/java/com/koinonia/igreja/core/util/Constants.kt
+++ app/src/main/java/com/koinonia/igreja/core/util/Constants.kt
@@ -4,7 +4,6 @@ object Constants {
     // Configurações do Supabase (Credenciais oficiais de produção)
     const val SUPABASE_URL = "https://wpgplnsopcqoldqalhrq.supabase.co"
     const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwZ3BsbnNvcGNxb2xkcWFsaHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMjY4MjAsImV4cCI6MjA5OTcwMjgyMH0.PuocS1VMFzEQrMeMgzeRt01UJEUl5Y5jtCTL9EeXU-s"
-    const val SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwZ3BsbnNvcGNxb2xkcWFsaHJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDEyNjgyMCwiZXhwIjoyMDk5NzAyODIwfQ.jY99mDBLUPyy_8NtkWCHEq0exmrZ8h41EMEqnxXsnK8"
 
     // Configurações de Fuso Horário
     const val SALVADOR_TIMEZONE = "America/Bahia"
